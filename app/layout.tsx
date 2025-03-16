3366efb0f6ce957d16dfd8f850a248414129234f
Jhamat Mahbubani <jhamatmahbubani@monzo.com>
2025-03-16 16:00:01 +0000

    fix: need to specify icon image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: "All-In Podcast Predictions",
   description: 'How accurate are the All-In Podcast hosts\' predictions?',
-  icons: {
-    icon: '/all-in.png',
-  },
 }
 
 export default function RootLayout({
@@ -23,10 +20,10 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/all-in.png" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <meta property="og:title" content="All-In Podcast Predictions" />
+        <meta property="og:title" content="All Prophets" />
         <meta property="og:description" content="How accurate are the All-In Podcast hosts' predictions?" />
-        <meta property="og:image" content="/all-in.png" />
-        <meta property="og:url" content="https://chamath-eight.vercel.app" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/jammygrams/chamath/main/public//all-in.png" />
+        <meta property="og:url" content="https://www.allprophets.org" />
       </head>
       <body className={`${inter.className} bg-gray-900 text-gray-100`}>{children}</body>
     </html>
